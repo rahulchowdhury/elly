c12b3fa88208b97d37e0d9ed11acd64a4a3b83da
Rahul Chowdhury <rahulchowdhurymail@gmail.com>
2019-07-24 09:27:47 +0530

    Complete elephant profile model

--- app/src/main/java/co/rahulchowdhury/elly/data/model/Elephant.kt
+++ app/src/main/java/co/rahulchowdhury/elly/data/model/Elephant.kt
@@ -4,5 +4,12 @@ data class Elephant(
     private val id: String,
     private val name: String,
     private val affiliation: String,
-    private val species: String
+    private val species: String,
+    private val sex: String,
+    private val fictional: String,
+    private val dob: String,
+    private val dod: String,
+    private val wikilink: String,
+    private val image: String,
+    private val note: String
 )
