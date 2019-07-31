096e86e0703197b7b7269772a439858e0432f470
Rahul Chowdhury <rahulchowdhurymail@gmail.com>
2019-07-31 09:14:01 +0530

    Add first repo test

--- app/src/test/java/co/rahulchowdhury/elly/data/repo/DefaultElephantRepositoryTest.kt
+++ app/src/test/java/co/rahulchowdhury/elly/data/repo/DefaultElephantRepositoryTest.kt
@@ -1,5 +1,6 @@
 package co.rahulchowdhury.elly.data.repo
 
+import co.rahulchowdhury.elly.data.model.local.Elephant
 import co.rahulchowdhury.elly.data.source.local.elephant.LocalElephantDataSource
 import co.rahulchowdhury.elly.data.source.remote.elephant.RemoteElephantDataSource
 import co.rahulchowdhury.elly.seed.elephant.seedElephant
@@ -9,18 +10,18 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
+import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class DefaultElephantRepositoryTest {
     @Mock
     lateinit var fakeLocalElephantDataSource: LocalElephantDataSource
-
     @Mock
     lateinit var fakeRemoteElephantDataSource: RemoteElephantDataSource
 
     lateinit var defaultElephantRepository: DefaultElephantRepository
+    private val elephant: Elephant = seedElephant
 
     @Before
     fun setUp() {
@@ -31,14 +32,23 @@ class DefaultElephantRepositoryTest {
     }
 
     @Test
-    fun `should get elephants from local source`() {
+    fun `should get elephants only from local source`() {
         runBlocking {
-            `when`(fakeLocalElephantDataSource.getElephants()).thenReturn(listOf(seedElephant))
+            `when`(fakeLocalElephantDataSource.getElephants()).thenReturn(listOf(elephant))
             `when`(fakeLocalElephantDataSource.hasStaleOrNoElephants()).thenReturn(false)
 
             val elephants = defaultElephantRepository.getElephants()
 
             assertThat(elephants).hasSize(1)
+            assertThat(elephants[0].name).isEqualTo(elephant.name)
+
+            verify(fakeLocalElephantDataSource, times(1)).hasStaleOrNoElephants()
+            verify(fakeRemoteElephantDataSource, never()).fetchElephants()
+            verify(fakeLocalElephantDataSource, never()).saveElephants(listOf())
+            verify(fakeLocalElephantDataSource, times(1)).getElephants()
+
+            verifyNoMoreInteractions(fakeLocalElephantDataSource)
+            verifyNoMoreInteractions(fakeRemoteElephantDataSource)
         }
     }
 }
